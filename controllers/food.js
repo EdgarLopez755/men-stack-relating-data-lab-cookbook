02e957830b9ad657aa2ea2f38bc3c795cf578c47
Edgar Lopez <edgarlopez755@gmail.com>
2024-09-24 21:31:36 -0700

    Add middleware

---
+++ controllers/food.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const User = require('../models/user.js')
+
+// router logic will go here <-------- (later)
+
+
+module.exports = router
+
+
+
+
