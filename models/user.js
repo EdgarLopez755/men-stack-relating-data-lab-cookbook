3cac78c241a98bf3d0ec10ce051d494076651d60
Edgar Lopez <edgarlopez755@gmail.com>
2024-09-24 20:07:28 -0700

    Add foodSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+  expirationDate: Date,
+  notes: String
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +16,8 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry:
+  [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
