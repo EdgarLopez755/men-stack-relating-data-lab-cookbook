e4583dc8604b43412da2ca88759f841b7972664b
Edgar Lopez <edgarlopez755@gmail.com>
2024-10-10 22:52:04 -0700

    finish pantry

--- controllers/users.js
+++ controllers/users.js
@@ -5,3 +5,37 @@ const User = require('../models/user.js')
 
 
 
+
+// router.get('/', async (req, res) => {
+//     const allUsers = await User.find()
+//     console.log(allUsers)
+//     res.render('users/index.ejs', {
+//         allUsers
+//     })
+// })
+
+
+router.get('/', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.render('users/index.ejs', { users });
+    } catch (error) {
+      console.error(error);
+      res.redirect('/');
+    }
+  });
+
+
+
+
+
+
+router.get('/:userId/', async (req, res) => {
+    const usersPage = await User.findById(req.params.userId)
+    res.render('users/show.ejs'), {usersPage}
+    
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const usersController = require('./controllers/users.js')
 
 
 const isSignedIn = require('./middleware/is-signed-in.js');
@@ -14,6 +15,7 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js')
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -40,32 +42,34 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/foods', (req,res) => {
-  res.render('/foods/new.ejs');
-});
+// app.get('/foods', (req,res) => {
+//   res.render('/foods/new.ejs');
+// });
 
-app.get('/foods/new', (req, res) => {
-  const user = req.session.user;
-  res.render('foods/new.ejs', { user });
-});
-
-
+// app.get('/foods/new', (req, res) => {
+//   const user = req.session.user;
+//   res.render('foods/new.ejs', { user });
+// });
 
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+
+
+// app.get('/vip-lounge', (req, res) => {
+//   if (req.session.user) {
+//     res.send(`Welcome to the party ${req.session.user.username}.`);
+//   } else {
+//     res.send('Sorry, no guests allowed.');
+//   }
+// });
 
 
 app.use(passUserToView)
 app.use('/auth', authController);
 app.use(isSignedIn)
-app.use('/users/:userId/foods',foodsController)
 app.use('/foods',foodsController)
+app.use('/:userId', usersController)
+app.use('/users/:userId/foods',foodsController)
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
